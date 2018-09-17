9750930cc1388a059be271ab01f08fc783c354b5
Omar Shafik <o.shafik13@gmail.com>
2018-09-17 21:16:44 +0200

    image gallery (Client/Server Approach)

--- Meteor/meteor_test/image_share/client/main.js
+++ Meteor/meteor_test/image_share/client/main.js
@@ -1,36 +1,56 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
-
+import { Mongo } from 'meteor/mongo';
+import { Meteor } from 'meteor/meteor';
 import './main.html';
+import { Images } from '../collections/images';
 
-if (Meteor.isClient) {
-  var img_data = [
-    {
-      img_src: "laptops.jpg",
-      img_alt: "laptops"
-    },
+Template.images.helpers({
+  images:
+    Images.find({}, { sort: { createdOn: -1, rating: -1 } })
+});
 
-    {
-      img_src: "bass.jpg",
-      img_alt: "bass"
-    },
+Template.images.events({
+  'click .js-image': function (event) {
+    $(event.target).css("width", "50px");
+  },
+  'click .js-del-image': function (event) {
+    var image_id = this._id;
+    console.log(image_id);
+    // use jquery to hide the image component
+    // then remove it at the end of the animation
+    $("#" + image_id).hide('slow', function () {
+      Images.remove({ "_id": image_id });
+    })
+  },
+  'click .js-rate-image': function (event) {
+    var rating = $(event.currentTarget).data("userrating");
+    console.log(rating);
+    var image_id = this.id;
+    console.log(image_id);
 
-    {
-      img_src: "beard.jpg",
-      img_alt: "beard"
-    },
-  ];
+    Images.update({ _id: image_id },
+      { $set: { rating: rating } });
+  },
+  'click .js-show-image-form': function (event) {
+    $("#image_add_form").modal('show');
+  }
 
-    Template.images.helpers({images:img_data});
+});
 
-    Template.images.events({
-      'click .js-image': function(event){
-        $(event.target).css("width", "50px");
-      }
+Template.image_add_form.events({
+  'submit .js-add-image': function (event) {
+    var img_src, img_alt;
+    img_src = event.target.img_src.value;
+    img_alt = event.target.img_alt.value;
+    console.log("src: " + img_src + " alt:" + img_alt);
+
+    Images.insert({
+      img_src: img_src,
+      img_alt: img_alt,
+      createdOn: new Date()
     });
-
-}
-
-if (Meteor.isServer) {
-  console.log("I am the Server");
-}
+    $("#image_add_form").modal('show');
+    return false;
+  }
+});
--- Meteor/meteor_test/image_share/server/main.js
+++ Meteor/meteor_test/image_share/server/main.js
@@ -1,5 +1,20 @@
 import { Meteor } from 'meteor/meteor';
+import { Mongo } from 'meteor/mongo';
+import { Images } from '../collections/images';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+  if (Images.find().count() == 0) {
+    for (var i = 1; i < 23; i++) {
+      Images.insert(
+        {
+          img_src: "img_" + i + ".jpg",
+          img_alt: "image number " + i
+        }
+      );
+    }// end of for insert images
+    // count the images!
+    console.log("startup.js says: " + Images.find().count());
+  }// end of if have no images
+
+  console.log(Images.find().count());
 });
